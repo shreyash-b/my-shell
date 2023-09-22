1cd19e3eb4f3adef0fe3f8be1d34de2e4b3885ca
Shreyash Bubane <>
2023-09-22 20:24:20 +0530

    added echo and cat functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+
+struct CmdDets<'a>{
+    cmd_name: &'a str,
+    callback_fn: Box<dyn Fn(String)>
+}
+
+fn main(){
+    let echo_cmd = CmdDets{
+        cmd_name: "echo",
+        callback_fn: Box::new(|arg| {
+            println!("ECHO: {}", arg);
+        })
+    };
+
+    (echo_cmd.callback_fn)(String::from("value"));
+
+
+    let cat_cmd = CmdDets{
+        cmd_name: "cat",
+        callback_fn: Box::new(|arg| {
+            let file_content = fs::read_to_string(&arg)
+                .expect("unable to read file");
+            
+            println!("CAT: {}", arg);
+            println!("{}", file_content);
+        })
+    };
+
+    (cat_cmd.callback_fn)(String::from("testfile.txt"));
+
 }
