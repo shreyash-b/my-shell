5a8d73a63ee8ce01ff8e9f43c721a20caafeec56
Shreyash Bubane <>
2023-09-22 22:43:25 +0530

    added shell interface

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
-mod shell_commands;
 use shell_commands::commands;
+use std::io::{self, Write};
 
-fn main(){
-    let filename = "/home/shreyash/testfile";
-    commands::cat_callback(String::from(filename));
+mod shell_commands;
+
+struct Shell {
+    shell_prefix: String,
+}
+
+impl Shell {
+    fn new(prefix: String) -> Self {
+        return Shell {
+            shell_prefix: prefix,
+        };
+    }
+
+    fn run(&self) {
+        let mut cmd = String::new();
+
+        print!("{}> ", self.shell_prefix);
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut cmd).unwrap();
+
+
+    }
+}
+
+fn main() {
+    let my_shell = Shell::new(String::from("my-shell"));
+    my_shell.run();
 }
