feeefd5bf882fd593073cf665487a828299c9c73
Swaraj Gosavi <106607805+swarajgosavi@users.noreply.github.com>
2023-09-28 13:31:55 +0530

    Added Error redirections 2> and 2>&1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use shell_commands::commands;
 use std::io::{self, Write};
+use std::fs::File;
+use std::path::Path;
 
 mod shell_commands;
 
@@ -7,6 +9,7 @@ struct Shell {
     shell_prefix: String,
 }
 
+#[allow(unused_assignments)]
 impl Shell {
     fn new(prefix: String) -> Self {
         return Shell {
@@ -53,11 +56,54 @@ impl Shell {
         for c in cmd{
             // assuming that pipe redirects output as command line argument instead of stdin for simplicity
 
-            let mut execute_cmd = c.to_string();
-            execute_cmd.push_str(out.as_str());
+            let execute_cmd = c.to_string();
+            let mut command = String::new();
+            let mut file_path = String::from("");
+            let mut err_out = false;
+
+            
+            if execute_cmd.contains("2>&1") {
+                command = execute_cmd[..execute_cmd.find("2>&1").unwrap()-1].to_string();
+                file_path = execute_cmd[execute_cmd.find("2>&1").unwrap()+5..execute_cmd.capacity()-2].to_string();
+                err_out = true;
+            }
+            else if execute_cmd.contains("2>") {
+                command = execute_cmd[..execute_cmd.find("2>").unwrap()-1].to_string();
+                file_path = execute_cmd[execute_cmd.find("2>").unwrap()+3..execute_cmd.capacity()-2].to_string();
+            }
+            else {
+                command = execute_cmd;
+            }
+
+
+
+            command.push_str(out.as_str());
             out.clear();
             err.clear();
-            ret_val = self.command_executor(&mut out, &mut err, execute_cmd);
+            ret_val = self.command_executor(&mut out, &mut err, command);
+
+            if !file_path.is_empty() {
+
+                let path = Path::new(&file_path);
+                let display = path.display();
+
+                let mut file = match File::create(&path) {
+                    Err(why) => panic!("couldn't create {}: {}", display, why),
+                    Ok(file) => file,
+                };
+
+                match file.write_all(err.as_bytes()) {
+                    Err(why) => panic!("couldn't write to {}: {}", display, why),
+                    Ok(_) => println!("successfully wrote to {}", display),
+                }
+
+                if err_out == true {
+                    match file.write_all(out.as_bytes()) {
+                        Err(why) => panic!("couldn't write to {}: {}", display, why),
+                        Ok(_) => println!("successfully wrote to {}", display),
+                    }
+                }
+            }
         }
 
         write!(io::stdout(), "{}", out).unwrap();
