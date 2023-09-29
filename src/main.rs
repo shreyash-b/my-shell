01196cb8ba1b8a105e9a6015b6389f5b65c585b1
Shreyash Bubane <>
2023-09-29 10:21:14 +0530

    fixed pipes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use shell_commands::commands;
-use std::io::{self, Write};
 use std::fs::OpenOptions;
+use std::io::{self, Write};
 use std::path::Path;
+
+use crate::shell_commands::commands::echo_callback;
 
 mod shell_commands;
 
@@ -17,31 +19,46 @@ impl Shell {
         };
     }
 
-    fn command_executor(&self, out: &mut String, err: &mut String, cmd:String) -> i32{
-
+    fn command_executor(
+        &self,
+        in_stream: &mut String,
+        out_stream: &mut String,
+        err_string: &mut String,
+        cmd: String,
+    ) -> i32 {
         let input_cmd = cmd.split_ascii_whitespace().collect::<Vec<_>>();
         let cmd = input_cmd[0];
         let arg = input_cmd[1..].join(" ");
 
         let mut ret_val: i32 = 0;
 
+        type func = fn(&mut String, &mut String, &mut String, &String) -> i32;
+
+        let mut exec_func: func = commands::echo_callback;
+        // exec_func = commands::echo_callback;
+
         match cmd {
-            "echo" => ret_val = commands::echo_callback(out, err, &arg),
-            "cat" => ret_val = commands::cat_callback(out, err, &arg),
-            "ls" => ret_val = commands::ls_callback(out, err, &arg),
+            "echo" => exec_func = commands::echo_callback,
+            "cat" => exec_func = commands::cat_callback,
+            "ls" => exec_func = commands::ls_callback,
             "exit" => ret_val = 11,
-            &_ => {}
+            &_ => {return 12}
+        }
+
+        if ret_val != 11{
+            exec_func(in_stream, out_stream, err_string, &arg);
         }
 
         return ret_val;
     }
 
-
     fn parse(&self, user_cmd: String) -> i32 {
-
-        let mut out = String::new(); // stdout stream
-        let mut err = String::new(); // stderr stream
+        let mut in_stream = String::new();
+        let mut out_stream = String::new(); // stdout stream
+        let mut err_stram = String::new(); // stderr stream
         let mut ret_val = 0;
+
+        // out2.push(2);
 
         // add features for pipelining and output redirection
         // others: modify the parse function by utilizing splitted user_cmd
@@ -50,14 +67,13 @@ impl Shell {
         // command_executor can only execute "cat /some/file" and "echo <args>" individually
         // pipelining and redirection is to be implemented in this function
 
-
         //pipelining
         let cmd = user_cmd.split(" | ");
         let mut to_append: bool = false;
-        let mut std_out: bool = false;  // Checks the standrad ouput
-        let mut std_err: bool = false;  // Checks the standrad error
+        let mut std_out: bool = false; // Checks the standrad ouput
+        let mut std_err: bool = false; // Checks the standrad error
 
-        for c in cmd{
+        for c in cmd {
             // assuming that pipe redirects output as command line argument instead of stdin for simplicity
 
             let mut execute_cmd = c.to_string();
@@ -67,43 +83,39 @@ impl Shell {
             std_err = false;
             std_out = false;
 
-            
-
-            if execute_cmd.contains(">>"){
+            if execute_cmd.contains(">>") {
                 let split: Vec<&str> = execute_cmd.split(">>").collect();
                 (command, file_path) = (split[0].trim().to_string(), split[1].trim().to_string());
                 std_out = true;
                 to_append = true;
-            }
-
-            else if execute_cmd.contains(">") {
+            } else if execute_cmd.contains(">") {
                 if execute_cmd.contains("2>&1") {
                     execute_cmd = execute_cmd.replace("2>&1", "");
                     let split: Vec<&str> = execute_cmd.split(">").collect();
-                    (command, file_path) = (split[0].trim().to_string(), split[1].trim().to_string());
+                    (command, file_path) =
+                        (split[0].trim().to_string(), split[1].trim().to_string());
                     std_err = true;
                     std_out = true;
-                }
-                else if execute_cmd.contains("2>") {
+                } else if execute_cmd.contains("2>") {
                     let split: Vec<&str> = execute_cmd.split("2>").collect();
-                    (command, file_path) = (split[0].trim().to_string(), split[1].trim().to_string());
+                    (command, file_path) =
+                        (split[0].trim().to_string(), split[1].trim().to_string());
                     std_err = true;
-                }
-                else{
+                } else {
                     let split: Vec<&str> = execute_cmd.split(">").collect();
-                    (command, file_path) = (split[0].trim().to_string(), split[1].trim().to_string());
+                    (command, file_path) =
+                        (split[0].trim().to_string(), split[1].trim().to_string());
                     std_out = true;
                 }
-            }
-
-            else {
+            } else {
                 command = execute_cmd;
             }
 
-            command.push_str(out.as_str());
-            out.clear();
-            err.clear();
-            ret_val = self.command_executor(&mut out, &mut err, command);
+            // command.push_str(out_stream.as_str());
+            in_stream = out_stream.clone();
+            out_stream.clear();
+            err_stram.clear();
+            ret_val = self.command_executor(&mut in_stream, &mut out_stream, &mut err_stram, command);
 
             if !file_path.is_empty() {
                 let path = Path::new(&file_path);
@@ -111,59 +123,54 @@ impl Shell {
                 let mut content = String::new();
 
                 if std_out == true {
-                    content += &out;
+                    content += &out_stream;
                 }
-        
+
                 if std_err == true {
-                    content += &err;
+                    content += &err_stram;
                 }
 
                 if to_append == false {
-                    self.handle_redirect(&mut content, path).unwrap_or_else(|why| {
-                        println!("! {:?}", why.kind());
-                    });
-                }
-
-                else if to_append == true {
-                    self.handle_redirect_append(&mut content, path).unwrap_or_else(|why| {
-                        println!("! {:?}", why.kind());
-                    });
+                    self.handle_redirect(&mut content, path)
+                        .unwrap_or_else(|why| {
+                            println!("! {:?}", why.kind());
+                        });
+                } else if to_append == true {
+                    self.handle_redirect_append(&mut content, path)
+                        .unwrap_or_else(|why| {
+                            println!("! {:?}", why.kind());
+                        });
                 }
             }
         }
 
         if std_err == false {
-            write!(io::stdout(), "{}", out).unwrap();
-            write!(io::stderr(), "{}", err).unwrap();
+            write!(io::stdout(), "{}", out_stream).unwrap();
+            write!(io::stderr(), "{}", err_stram).unwrap();
         }
 
         return ret_val;
     }
 
-    fn handle_redirect(&self, s: &str, path: &Path) -> io::Result<()> {     // Function to handle redirects takes content and file path as arguments
+    fn handle_redirect(&self, s: &str, path: &Path) -> io::Result<()> {
+        // Function to handle redirects takes content and file path as arguments
         let mut f = OpenOptions::new()
-                    .create(true)
-                    .truncate(true)
-                    .write(true)
-                    .open(path)?;
-        
-        
+            .create(true)
+            .truncate(true)
+            .write(true)
+            .open(path)?;
+
         f.write_all(s.as_bytes())
     }
 
-    fn handle_redirect_append(&self, s: &str, path: &Path) -> io::Result<()> {     // Function to handle redirects takes content and file path as arguments
-        let mut f = OpenOptions::new()
-                    .create(true)
-                    .append(true)
-                    .open(path)?;
-        
-        
+    fn handle_redirect_append(&self, s: &str, path: &Path) -> io::Result<()> {
+        // Function to handle redirects takes content and file path as arguments
+        let mut f = OpenOptions::new().create(true).append(true).open(path)?;
+
         f.write_all(s.as_bytes())
     }
-
 
     fn run(&self) {
-        
         loop {
             let mut cmd = String::new();
             print!("{}> ", self.shell_prefix);
@@ -171,7 +178,7 @@ impl Shell {
             io::stdin().read_line(&mut cmd).unwrap();
 
             let ret_val = self.parse(cmd);
-            if ret_val == 11{
+            if ret_val == 11 {
                 break;
             }
         }
--- src/shell_commands.rs
+++ src/shell_commands.rs
@@ -3,30 +3,36 @@
 pub mod commands {
     use std::fmt::Write;
     use std::fs;
+    // use std::io::Write;
 
 
-    pub fn echo_callback(out: &mut String, err: &mut String, arg: &str) -> i32 {
-        writeln!(out, "{}", arg).expect("Unable to write");
+    pub fn echo_callback(in_stream: &mut String, out_stream: &mut String, err_stream: &mut String, arg: &String) -> i32 {
+        // let mut write_str = arg.clone();
+        writeln!(out_stream, "{}", in_stream).expect("Unable to write");
+        if arg.len() != 0 {
+            writeln!(out_stream, "{}", arg).expect("Unable to write");
+            // write_str = in_stream.clone();
+        } 
         return 0;
     }
 
-    pub fn cat_callback(out: &mut String, err: &mut String, arg: &str) -> i32 {
+    pub fn cat_callback(in_stream: &mut String, out_stream: &mut String, err_stream: &mut String, arg: &String) -> i32 {
         let file_contents = match fs::read_to_string(&arg) {
             Ok(v) => v,
             Err(_) => {
-                writeln!(err, "[ERROR] Unable to read {}", arg).unwrap();
+                writeln!(err_stream, "[ERROR] Unable to read {}", arg).unwrap();
                 return -1;
             }
         };
-        writeln!(out, "{}", file_contents).unwrap();
+        writeln!(out_stream, "{}", file_contents).unwrap();
         return 0;
     }
 
-    pub fn ls_callback(out: &mut String, err: &mut String, arg: &str) -> i32 {
+    pub fn ls_callback(in_stream: &mut String, out_stream: &mut String, err_stream: &mut String, arg: &String) -> i32 {
         let paths = fs::read_dir("./").unwrap();
 
         for path in paths {
-            writeln!(out, "{}", path.unwrap().path().display()).unwrap();
+            writeln!(out_stream, "{}", path.unwrap().path().display()).unwrap();
         }
 
         return 0;
